8e947d50e6a290854c96514189cb2f8e8be64f97
Enes Kızılcin <enes.kizilcin@gmail.com>
2022-10-29 15:37:14 +0300

    auth NewService test is written.

---
+++ internal/auth/service_test.go
@@ -0,0 +1,16 @@
+package auth_test
+
+import (
+	"github.com/eneskzlcn/currency-conversion-service/internal/config"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGivenJWTConfigThenItShouldReturnNewServiceWhenNewServiceCalled(t *testing.T) {
+	givenConfig := config.Jwt{
+		ATPrivateKey:        "private",
+		ATExpirationSeconds: 200,
+	}
+	authService := auth.NewService(givenConfig)
+	assert.NotNil(t, authService)
+}
