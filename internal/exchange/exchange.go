c1d5305170a743385c30549507af345e53c026c7
Enes Kızılcin <enes.kizilcin@gmail.com>
2022-10-30 19:11:08 +0300

    exchange models created.

---
+++ internal/exchange/exchange.go
@@ -0,0 +1,20 @@
+package exchange
+
+import "time"
+
+const (
+	ExchangeRateExpirationMinutes = 3
+)
+
+type ExchangeRateRequest struct {
+	FromCurrency string `json:"from_currency"`
+	ToCurrency   string `json:"to_currency"`
+}
+
+type ExchangeRateResponse struct {
+	FromCurrency string    `json:"from_currency"`
+	ToCurrency   string    `json:"to_currency"`
+	ExchangeRate float32   `json:"exchange_rate"`
+	CreatedAt    time.Time `json:"created_at"`
+	ExpiresAt    time.Time `json:"expires_at"`
+}
