3b475fddb361b66708da23154e3590923ab2bec1
Enes Kızılcin <enes.kizilcin@gmail.com>
2022-10-30 19:11:42 +0300

    exchange handler GetExchangeRate test wrote and then implemented.

--- internal/exchange/handler.go
+++ internal/exchange/handler.go
@@ -1,24 +1,39 @@
 package exchange
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+	"github.com/gofiber/fiber/v2"
+)
 
 type ExchangeService interface {
+	CreateExchangeRate(ctx context.Context, request ExchangeRateRequest) (ExchangeRateResponse, error)
+}
+type AuthGuard interface {
+	ProtectWithJWT(handler fiber.Handler) fiber.Handler
 }
 type Handler struct {
 	exchangeService ExchangeService
+	authGuard       AuthGuard
 }
 
-func NewHandler(service ExchangeService) *Handler {
-	if service == nil {
+func NewHandler(service ExchangeService, guard AuthGuard) *Handler {
+	if service == nil || guard == nil {
 		return nil
 	}
-	return &Handler{exchangeService: service}
+	return &Handler{exchangeService: service, authGuard: guard}
 }
 func (h *Handler) GetExchangeRate(ctx *fiber.Ctx) error {
-	return nil
+	var request ExchangeRateRequest
+	if err := ctx.BodyParser(&request); err != nil {
+		return ctx.SendStatus(fiber.StatusBadRequest)
+	}
+	exchangeRate, err := h.exchangeService.CreateExchangeRate(ctx.Context(), request)
+	if err != nil {
+		return ctx.SendStatus(fiber.StatusInternalServerError)
+	}
+	return ctx.Status(fiber.StatusCreated).JSON(exchangeRate)
 }
 func (h *Handler) RegisterRoutes(app *fiber.App) {
 	appGroup := app.Group("/exchange")
-	appGroup.Get("/rate", h.GetExchangeRate)
-
+	appGroup.Get("/rate", h.authGuard.ProtectWithJWT(h.GetExchangeRate))
 }
--- internal/exchange/handler_test.go
+++ internal/exchange/handler_test.go
@@ -1,6 +1,8 @@
 package exchange_test
 
 import (
+	"errors"
+	"github.com/eneskzlcn/currency-conversion-service/internal/common/testutil"
 	"github.com/eneskzlcn/currency-conversion-service/internal/exchange"
 	mocks "github.com/eneskzlcn/currency-conversion-service/internal/mocks/exchange"
 	"github.com/gofiber/fiber/v2"
@@ -8,31 +10,88 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestNewHandler(t *testing.T) {
-	t.Run("given currency service then it should return new handler", func(t *testing.T) {
+	t.Run("given currency service and auth guard then it should return new handler", func(t *testing.T) {
 		ctrl := gomock.NewController(t)
 		mockExchangeService := mocks.NewMockExchangeService(ctrl)
-		handler := exchange.NewHandler(mockExchangeService)
+		mockAuthGuard := mocks.NewMockAuthGuard(ctrl)
+		handler := exchange.NewHandler(mockExchangeService, mockAuthGuard)
 		assert.NotNil(t, handler)
 	})
-	t.Run("given currency service then it should return new handler", func(t *testing.T) {
-		handler := exchange.NewHandler(nil)
+	t.Run("given empty service or auth guard then it should return nil", func(t *testing.T) {
+		handler := exchange.NewHandler(nil, nil)
+		assert.Nil(t, handler)
+		handler = exchange.NewHandler(nil, mocks.NewMockAuthGuard(gomock.NewController(t)))
+		assert.Nil(t, handler)
+		handler = exchange.NewHandler(mocks.NewMockExchangeService(gomock.NewController(t)), nil)
 		assert.Nil(t, handler)
 	})
 }
 func TestHandler_RegisterRoutes(t *testing.T) {
 	app := fiber.New()
-	ctrl := gomock.NewController(t)
-	mockExchangeService := mocks.NewMockExchangeService(ctrl)
-	handler := exchange.NewHandler(mockExchangeService)
+	handler, _, mockAuthGuard := createHandlerWithMockExchangeServiceAndAuthGuard(t)
+	mockAuthGuard.EXPECT().ProtectWithJWT(gomock.Any()).Return(func(ctx *fiber.Ctx) error { return nil })
 	handler.RegisterRoutes(app)
 
 	assertRouteRegistered(t, app, fiber.MethodGet, "/exchange/rate")
 }
+func TestHandler_GetExchangeRate(t *testing.T) {
+	app := fiber.New()
+	handler, mockExchangeService, _ := createHandlerWithMockExchangeServiceAndAuthGuard(t)
+	app.Get("/rate", handler.GetExchangeRate)
+	t.Run("given not valid exchange rate request then it should return bad request", func(t *testing.T) {
+		givenRequest := "notvalidexchangerate"
+		req := testutil.MakeTestRequestWithBody(fiber.MethodGet, "/rate", givenRequest)
+		resp, err := app.Test(req)
+		assert.Nil(t, err)
+		assert.Equal(t, fiber.StatusBadRequest, resp.StatusCode)
+	})
+	t.Run("given valid exchange rate request but unexpected error occurred on service then return status internal server error", func(t *testing.T) {
+		givenRequest := exchange.ExchangeRateRequest{
+			FromCurrency: "TRY",
+			ToCurrency:   "USD",
+		}
+		mockExchangeService.EXPECT().CreateExchangeRate(gomock.Any(), givenRequest).
+			Return(exchange.ExchangeRateResponse{}, errors.New(""))
+
+		req := testutil.MakeTestRequestWithBody(fiber.MethodGet, "/rate", givenRequest)
+		resp, err := app.Test(req)
+		assert.Nil(t, err)
+		assert.Equal(t, fiber.StatusInternalServerError, resp.StatusCode)
+	})
+	t.Run("given valid exchange rate then it should return exchange rate response with status created", func(t *testing.T) {
+		givenRequest := exchange.ExchangeRateRequest{
+			FromCurrency: "TRY",
+			ToCurrency:   "USD",
+		}
+		expectedResponse := exchange.ExchangeRateResponse{
+			FromCurrency: "TRY",
+			ToCurrency:   "USD",
+			ExchangeRate: 0.23,
+			CreatedAt:    time.Now(),
+			ExpiresAt:    time.Now().Add(exchange.ExchangeRateExpirationMinutes * time.Minute),
+		}
+		mockExchangeService.EXPECT().CreateExchangeRate(gomock.Any(), givenRequest).
+			Return(expectedResponse, nil)
+
+		req := testutil.MakeTestRequestWithBody(fiber.MethodGet, "/rate", givenRequest)
+		resp, err := app.Test(req)
+		assert.Nil(t, err)
+		assert.Equal(t, fiber.StatusCreated, resp.StatusCode)
+		testutil.AssertBodyEqual(t, resp.Body, expectedResponse)
+	})
+}
 func assertRouteRegistered(t *testing.T, app *fiber.App, method, route string) {
-	resp, err := app.Test(httptest.NewRequest(fiber.MethodGet, "/exchange/rate", nil))
+	resp, err := app.Test(httptest.NewRequest(method, route, nil))
 	assert.Nil(t, err)
 	assert.NotEqual(t, fiber.StatusNotFound, resp.StatusCode)
+}
+func createHandlerWithMockExchangeServiceAndAuthGuard(t *testing.T) (*exchange.Handler, *mocks.MockExchangeService, *mocks.MockAuthGuard) {
+	ctrl := gomock.NewController(t)
+	mockExchangeService := mocks.NewMockExchangeService(ctrl)
+	mockAuthGuard := mocks.NewMockAuthGuard(ctrl)
+	return exchange.NewHandler(mockExchangeService, mockAuthGuard), mockExchangeService, mockAuthGuard
 }
