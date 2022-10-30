e71301f0a52b77b3a34452197f946cd3829ca62c
Enes Kızılcin <enes.kizilcin@gmail.com>
2022-10-31 00:32:00 +0300

    Conversion offer structure created.

---
+++ internal/conversion/offer.go
@@ -0,0 +1,12 @@
+package conversion
+
+import "time"
+
+type CurrencyConversionOfferRequest struct {
+	FromCurrency string    `json:"from_currency"`
+	ToCurrency   string    `json:"to_currency"`
+	ExchangeRate float32   `json:"exchange_rate"`
+	CreatedAt    time.Time `json:"created_at"`
+	ExpiresAt    time.Time `json:"expires_at"`
+	Balance      float32   `json:"balance"`
+}
