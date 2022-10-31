32b05fbfcc13f44a4b306d3898aef4e4166c2925
Enes Kızılcin <enes.kizilcin@gmail.com>
2022-10-31 18:02:59 +0300

    wallet repository GetUserWalletAccounts tested and implemented

--- internal/wallet/repository.go
+++ internal/wallet/repository.go
@@ -3,6 +3,7 @@ package wallet
 import (
 	"context"
 	"database/sql"
+	"github.com/eneskzlcn/currency-conversion-service/internal/entity"
 )
 
 type Repository struct {
@@ -16,11 +17,38 @@ func NewRepository(db *sql.DB) *Repository {
 	return &Repository{db: db}
 }
 func (r *Repository) IsUserWithUserIDExists(ctx context.Context, userID int) (bool, error) {
-	query := `SELECT EXISTS (SELECT 1 FROM users WHERE id = $1`
+	query := `SELECT EXISTS (SELECT 1 FROM users WHERE id = $1)`
 	row := r.db.QueryRowContext(ctx, query, userID)
 	var exists bool
 	if err := row.Scan(&exists); err != nil {
 		return false, err
 	}
 	return exists, nil
+}
+func (r *Repository) GetUserWalletAccounts(ctx context.Context, userID int) ([]entity.UserWallet, error) {
+	query := `
+		SELECT user_id, currency_code, balance, created_at, updated_at
+		FROM user_wallets uw 
+		WHERE uw.user_id = $1`
+	rows, err := r.db.QueryContext(ctx, query, userID)
+	if err != nil {
+		return nil, err
+	}
+	userWallets := make([]entity.UserWallet, 0)
+	for rows.Next() {
+		var wallet entity.UserWallet
+		err = rows.Scan(&wallet.UserID,
+			&wallet.Currency, &wallet.Balance, &wallet.CreatedAt,
+			&wallet.UpdatedAt)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if err = rows.Close(); err != nil {
+		return nil, err
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return userWallets, nil
 }
--- internal/wallet/repository_test.go
+++ internal/wallet/repository_test.go
@@ -2,11 +2,13 @@ package wallet_test
 
 import (
 	"context"
+	"github.com/eneskzlcn/currency-conversion-service/internal/entity"
 	"github.com/eneskzlcn/currency-conversion-service/internal/wallet"
 	"github.com/eneskzlcn/currency-conversion-service/postgres"
 	"github.com/stretchr/testify/assert"
 	"regexp"
 	"testing"
+	"time"
 )
 
 func TestNewRepository(t *testing.T) {
@@ -21,7 +23,7 @@ func TestNewRepository(t *testing.T) {
 func TestRepository_IsUserWithUserIDExists(t *testing.T) {
 	db, sqlmock := postgres.NewMockPostgres()
 	repository := wallet.NewRepository(db)
-	query := regexp.QuoteMeta(`SELECT EXISTS (SELECT 1 FROM users WHERE id = $1`)
+	query := regexp.QuoteMeta(`SELECT EXISTS (SELECT 1 FROM users WHERE id = $1)`)
 	t.Run("given existing user id then it should return true ", func(t *testing.T) {
 		userID := 2
 		expectedRows := sqlmock.NewRows([]string{"exists"}).AddRow(true)
@@ -41,5 +43,48 @@ func TestRepository_IsUserWithUserIDExists(t *testing.T) {
 		assert.Nil(t, sqlmock.ExpectationsWereMet())
 		assert.Nil(t, err)
 		assert.False(t, exists)
+	})
+}
+func TestRepository_GetUserWalletAccounts(t *testing.T) {
+	db, sqlmock := postgres.NewMockPostgres()
+	repository := wallet.NewRepository(db)
+	query := regexp.QuoteMeta(`SELECT user_id, currency_code, balance, created_at, updated_at
+			FROM user_wallets uw WHERE uw.user_id = $1`)
+
+	t.Run("given user that not has wallet account then it should return empty wallet account array", func(t *testing.T) {
+		userID := 3
+		expectedRows := sqlmock.NewRows([]string{"user_id", "currency_code", "balance", "created_at", "updated_at"})
+		sqlmock.ExpectQuery(query).WithArgs(userID).WillReturnRows(expectedRows)
+		userWallets, err := repository.GetUserWalletAccounts(context.TODO(), userID)
+		assert.Nil(t, err)
+		assert.Empty(t, userWallets)
+	})
+	t.Run("given user that has wallet accounts then it should return wallet accounts array", func(t *testing.T) {
+		userID := 4
+		expectedRows := sqlmock.NewRows([]string{"user_id", "currency_code", "balance", "created_at", "updated_at"})
+		expectedUserWallets := []entity.UserWallet{
+			{
+				UserID:    userID,
+				Currency:  "TRY",
+				Balance:   200,
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			},
+			{
+				UserID:    userID,
+				Currency:  "USD",
+				Balance:   300,
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			},
+		}
+		for _, userWallet := range expectedUserWallets {
+			expectedRows.AddRow(userWallet.UserID, userWallet.Currency,
+				userWallet.Balance, userWallet.CreatedAt, userWallet.UpdatedAt)
+		}
+		sqlmock.ExpectQuery(query).WithArgs(userID).WillReturnRows(expectedRows)
+		userWallets, err := repository.GetUserWalletAccounts(context.TODO(), userID)
+		assert.Nil(t, err)
+		assert.Empty(t, userWallets)
 	})
 }
