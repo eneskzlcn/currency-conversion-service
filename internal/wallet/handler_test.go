e702ba9ceb6b67c5bec36ee147169c909b9f7132
Enes Kızılcin <enes.kizilcin@gmail.com>
2022-10-31 16:14:54 +0300

    handler RegisterRoutes tested.

--- internal/wallet/handler_test.go
+++ internal/wallet/handler_test.go
@@ -1,8 +1,10 @@
 package wallet_test
 
 import (
+	"github.com/eneskzlcn/currency-conversion-service/internal/common/testutil"
 	mocks "github.com/eneskzlcn/currency-conversion-service/internal/mocks/wallet"
 	"github.com/eneskzlcn/currency-conversion-service/internal/wallet"
+	"github.com/gofiber/fiber/v2"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -19,6 +21,20 @@ func TestNewHandler(t *testing.T) {
 	})
 	t.Run("given valid arguments then it should return new Handler", func(t *testing.T) {
 		handler := wallet.NewHandler(mockWalletService, mockAuthGuard)
-		assert.Nil(t, handler)
+		assert.NotNil(t, handler)
 	})
+}
+func TestHandler_RegisterRoutes(t *testing.T) {
+	app := fiber.New()
+	handler, _, mockAuthGuard := createHandlerWithMockWalletServiceAndAuthGuard(t)
+	mockAuthGuard.EXPECT().ProtectWithJWT(gomock.Any()).Return(func(ctx *fiber.Ctx) error { return nil })
+	handler.RegisterRoutes(app)
+	testutil.AssertRouteRegistered(t, app, fiber.MethodGet, "/wallets")
+}
+
+func createHandlerWithMockWalletServiceAndAuthGuard(t *testing.T) (*wallet.Handler, *mocks.MockWalletService, *mocks.MockAuthGuard) {
+	ctrl := gomock.NewController(t)
+	mockWalletService := mocks.NewMockWalletService(ctrl)
+	mockAuthGuard := mocks.NewMockAuthGuard(ctrl)
+	return wallet.NewHandler(mockWalletService, mockAuthGuard), mockWalletService, mockAuthGuard
 }
